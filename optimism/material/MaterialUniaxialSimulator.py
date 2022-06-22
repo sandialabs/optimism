df146c4cae816cf83bf2a14fc15ebb5ce5c3b34e
Brandon Talamini <talamini1@llnl.gov>
2022-06-22 16:31:53 -0700

    Make uniaxial test a free function since the class wasn't buying us anything

--- optimism/material/MaterialUniaxialSimulator.py
+++ optimism/material/MaterialUniaxialSimulator.py
@@ -9,91 +9,83 @@ from optimism import TensorMath
 
 UniaxialOutput = namedtuple('UniaxialOutput', ['time', 'strainHistory', 'stressHistory',
                                                'energyHistory', 'internalVariableHistory'])
+UniaxialOutput.__doc__ = """\
+Output from a uniaxial tension test on a material.
+
+Attributes
+----------
+time: array
+    discrete time points in the unixial data
+strainHistory: array
+    displacement gradient tensor at each time point
+stressHistory: array
+    Piola stress tensor at each time point
+energyHistory: array
+    Potential function value at each time point
+internalVariableHistory: array
+    Collection of internal variables for the given material at each time point.
+"""
 
 
-class MaterialUniaxialSimulator:
-    """Generates the uniaxial response of a given potential density function
-
-    The non-axial strain components are determined by minimizing the potential,
-    which coincides with stress-free conditions for every stress component
-    besides the uniaxial stress.
-
-    Methods
+def run(materialModel, strain_history, maxTime, steps=10):
+    """ Generates the uniaxial response of a given material
+    Args
+    ----
+    materialModel: MaterialModel object
+        Material to subject to uniaxial stress test
+    strain_history: callable (float) -> float
+        The tensile strain as a function of time
+    maxTime: float
+        Upper limit of the time interval
+    steps: int
+        Number of time steps to take
+    
+    Returns
     -------
-    run():
-      Launches the simluation with the given parameters
+    UniaxialOutput: named tuple of constitutive output
     """
-
-    def __init__(self, materialModel, strain_history, maxTime, steps=10):
-        """Constructor
-
-        Args
-        ----
-        materialModel: MaterialModel object
-            Material to subject to uniaxial stress test
-        strain_history: callable (float) -> float
-            The tensile strain as a function of time
-        maxTime: float
-            Upper limit of the time interval
-        steps: int
-            Number of time steps to take
-        """
-        self.steps = steps
-        self.times = np.linspace(0.0, maxTime, num=steps)
-        self.uniaxialStrainHistory = strain_history(self.times)
-        self.energy_density = materialModel.compute_energy_density
-        self.converged_energy_density_and_stress = jit(value_and_grad(materialModel.compute_output_energy_density))
-        self.update = jit(materialModel.compute_state_new)
-        self.compute_initial_state = materialModel.compute_initial_state
-    
-    def run(self):
-        """ Execute the uniaxial test
-        Returns
-        -------
-        UniaxialOutput: named tuple of constitutive output
-        """
+    timePoints = np.linspace(0.0, maxTime, num=steps)
+    uniaxialStrainHistory = strain_history(timePoints)
+    energy_density = materialModel.compute_energy_density
+    converged_energy_density_and_stress = jit(value_and_grad(materialModel.compute_output_energy_density))
+    update = jit(materialModel.compute_state_new)
         
-        def obj_func(freeStrains, p):
-            strain = self.makeStrainTensor(freeStrains, p)
-            return self.energy_density(strain, p[1])
+    def obj_func(freeStrains, p):
+        strain = makeStrainTensor_(freeStrains, p)
+        return energy_density(strain, p[1])
 
-        solverSettings = EqSolver.get_settings()
-        internalVariables =  self.compute_initial_state()
-        freeStrains = np.zeros(2)
+    solverSettings = EqSolver.get_settings()
+    internalVariables =  materialModel.compute_initial_state()
+    freeStrains = np.zeros(2)
         
-        p = Objective.Params(self.uniaxialStrainHistory[0], internalVariables)
-        o = Objective.Objective(obj_func, freeStrains, p)
+    p = Objective.Params(uniaxialStrainHistory[0], internalVariables)
+    o = Objective.Objective(obj_func, freeStrains, p)
 
-        strainHistory = []
-        stressHistory = []
-        kirchhoffStressHistory = []
-        energyHistory = []
-        internalVariableHistory = []
-        for i in range(self.steps):
-            p = Objective.param_index_update(p, 0, self.uniaxialStrainHistory[i])
+    strainHistory = []
+    stressHistory = []
+    kirchhoffStressHistory = []
+    energyHistory = []
+    internalVariableHistory = []
+    for i in range(steps):
+        p = Objective.param_index_update(p, 0, uniaxialStrainHistory[i])
 
-            freeStrains = EqSolver.nonlinear_equation_solve(o, freeStrains, p, solverSettings, useWarmStart=True)
-            strain = self.makeStrainTensor(freeStrains, p)
-            internalVariables = self.update(strain, internalVariables)
-            energyDensity,stress = self.converged_energy_density_and_stress(strain, internalVariables)
-            p = Objective.param_index_update(p, 1, internalVariables)
-            o.p = p
+        freeStrains = EqSolver.nonlinear_equation_solve(o, freeStrains, p, solverSettings, useWarmStart=True)
+        strain = makeStrainTensor_(freeStrains, p)
+        internalVariables = update(strain, internalVariables)
+        energyDensity,stress = converged_energy_density_and_stress(strain, internalVariables)
+        p = Objective.param_index_update(p, 1, internalVariables)
+        o.p = p
 
-            F = np.identity(3) + strain
-            J = np.linalg.det(F)
-            cauchy = stress@F.T/J
-
-            strainHistory.append(strain)
-            stressHistory.append(stress)
-            energyHistory.append(energyDensity)
-            internalVariableHistory.append(internalVariables)
+        strainHistory.append(strain)
+        stressHistory.append(stress)
+        energyHistory.append(energyDensity)
+        internalVariableHistory.append(internalVariables)
             
-        return UniaxialOutput(onp.array(self.times), onp.array(strainHistory),
-                              onp.array(stressHistory), onp.array(energyHistory),
-                              onp.array(internalVariableHistory))
+    return UniaxialOutput(onp.array(timePoints), onp.array(strainHistory),
+                          onp.array(stressHistory), onp.array(energyHistory),
+                          onp.array(internalVariableHistory))
 
     
-    @staticmethod
-    def makeStrainTensor(freeStrains, p):
-        uniaxialStrain = p[0]
-        return np.diag(np.hstack((uniaxialStrain, freeStrains)))
+def makeStrainTensor_(freeStrains, p):
+    uniaxialStrain = p[0]
+    return np.diag(np.hstack((uniaxialStrain, freeStrains)))
--- optimism/material/test/testMaterialUniaxialSimulator.py
+++ optimism/material/test/testMaterialUniaxialSimulator.py
@@ -1,19 +1,19 @@
 import unittest
 
 from optimism.JaxConfig import *
-from optimism.material.MaterialUniaxialSimulator import MaterialUniaxialSimulator
+from optimism.material import MaterialUniaxialSimulator
 from optimism.material import LinearElastic
 from optimism.test.TestFixture import TestFixture
 
 
 class MaterialUniaxialSimulatorFixture(TestFixture):
 
-    def setUp(self):
-        self.E = 100.0
-        self.nu = 0.25
+    def test_uniaxial_state_achieved(self):
+        E = 100.0
+        nu = 0.25
 
-        properties = {"elastic modulus": self.E,
-                      "poisson ratio": self.nu,
+        properties = {"elastic modulus": E,
+                      "poisson ratio": nu,
                       "strain measure": "logarithmic"}
         material = LinearElastic.create_material_model_functions(properties)
         engineering_strain_rate = 1e-3
@@ -22,15 +22,8 @@ class MaterialUniaxialSimulatorFixture(TestFixture):
             return engineering_strain_rate*t
 
         maxTime = 1000.0
-        
-        self.uniaxialTester = MaterialUniaxialSimulator(
-            material, strain_history, maxTime, steps=20)
+        response = MaterialUniaxialSimulator.run(material, strain_history, maxTime, steps=20)
 
-
-    def test_uniaxial_state_achieved(self):
-        response = self.uniaxialTester.run()
-
-        print(response.stressHistory)
         for stress in response.stressHistory[1:]:
             self.assertGreater(stress[0,0], 0.0) # axial stress is nonnegative
             self.assertTrue(np.abs(np.all(stress.ravel()[1:]) < 1e-8)) # all other components near zero
