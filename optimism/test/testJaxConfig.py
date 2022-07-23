592550c634d343556eb7cc97f01c62fa4b3ac0fd
Brandon Talamini <talamini1@llnl.gov>
2022-07-23 09:36:34 -0700

    Make resaons for test failures clearer when checking if debugging options have been left on
    This is to guard against optional Jax debugging options being
    inadvertently checked in.

--- optimism/test/testJaxConfig.py
+++ optimism/test/testJaxConfig.py
@@ -13,11 +13,17 @@ class TestJaxConfiguration(TestFixture.TestFixture):
         self.assertTrue(a.dtype == np.float64)
 
     def test_debug_nans_is_off(self):
-        np.log(-1.0)
+        try:
+            np.log(-1.0)
+        except FloatingPointError:
+            self.fail("Floating point exception raised. Check to see if 'jax_debug_nans' has been left activated from a debugging session.")
 
     def test_debug_infs_is_off(self):
-        a = np.array([1.0])
-        a/0
+        try:
+            a = np.array([1.0])
+            a/0
+        except FloatingPointError:
+            self.fail("Floating point exception raised. Check to see if 'jax_debug_infs' has been left activated from a debugging session.")
 
     def test_jit_is_enabled(self):
         self.assertFalse(config.jax_disable_jit)
