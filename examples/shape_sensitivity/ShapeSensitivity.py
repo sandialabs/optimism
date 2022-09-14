3a6163c365e62f54d31fb8b22d566f577def78b0
Brandon Talamini <talamini1@llnl.gov>
2022-09-14 13:26:45 -0700

    Start a mode I fracture shape sensitivity example

---
+++ examples/shape_sensitivity/ShapeSensitivity.py
@@ -0,0 +1,118 @@
+import jax
+import jax.numpy as np
+
+from optimism import EquationSolver
+from optimism import FunctionSpace
+from optimism.material import LinearElastic
+from optimism import Mechanics
+from optimism import Mesh
+from optimism import Objective
+from optimism import ReadExodusMesh
+from optimism import SparseMatrixAssembler
+from optimism import QuadratureRule
+from optimism import VTKWriter
+
+# set up the mesh
+mesh = ReadExodusMesh.read_exodus_mesh('crack_domain.g')
+
+kFieldXOffset = mesh.coords[mesh.nodeSets['crack_tip'], 0]
+
+order = 2*mesh.masterElement.degree
+quadRule = QuadratureRule.create_quadrature_rule_on_triangle(degree=2*(order - 1))
+fs = FunctionSpace.construct_function_space(mesh, quadRule)
+
+ebcs = [Mesh.EssentialBC(nodeSet='external', field=0),
+        Mesh.EssentialBC(nodeSet='external', field=1),
+        Mesh.EssentialBC(nodeSet='ysymm', field=1)]
+
+fieldShape = Mesh.num_nodes(mesh), 2
+essentialBCManager = Mesh.DofManager(mesh, fieldShape, ebcs)
+
+E = 1.0
+nu = 0.25
+G = 0.5*E/(1 + nu)
+props = {"elastic modulus": E,
+         "poisson ratio": nu}
+material = LinearElastic.create_material_model_functions(props)
+
+solidMechanics = Mechanics.create_mechanics_functions(fs, "plane strain", material)
+
+def apply_mode_I_field_at_point(X, K_I):
+    R = np.linalg.norm(X)
+    theta = np.arctan2(X[1], X[0])    
+    u0 = K_I/G*np.sqrt(R/2.0/np.pi)*(1.0 - 2.0*nu + np.sin(0.5*theta)**2)*np.cos(0.5*theta)
+    u1 = K_I/G*np.sqrt(R/2.0/np.pi)*(2.0 - 2.0*nu - np.cos(0.5*theta)**2)*np.sin(0.5*theta)
+    return np.array([u0, u1])
+
+def apply_mode_I_field(coords, K_I):
+    return jax.vmap(apply_mode_I_field_at_point, (0, None))(coords, K_I)
+
+def get_ubcs(p):
+    appliedK = p[0]
+    X = mesh.coords[mesh.nodeSets['external'], :]
+    X = X.at[:, 0].add(-kFieldXOffset)
+    U = apply_mode_I_field(X, appliedK)
+    V = np.zeros_like(mesh.coords).at[mesh.nodeSets['external'], :].set(U)
+    return essentialBCManager.get_bc_values(V)
+
+def create_field(Uu, p):
+    return essentialBCManager.create_field(Uu, get_ubcs(p))
+
+# write the energy to minimize
+def potential_energy(Uu, p):
+    U = create_field(Uu, p)
+    internalVariables = p[1]
+    return solidMechanics.compute_strain_energy(U, internalVariables)
+
+def assemble_sparse_preconditioner_matrix(Uu, p):
+    U = create_field(Uu, p)
+    internalVariables = p[1]
+    elementStiffnesses = solidMechanics.compute_element_stiffnesses(U, internalVariables)
+    return SparseMatrixAssembler.assemble_sparse_stiffness_matrix(
+        elementStiffnesses, mesh.conns, essentialBCManager)
+
+def write_output(U, p, step):
+    vtkFilename = "crack_mode_1-" + str(step).zfill(3)
+    writer = VTKWriter.VTKWriter(mesh, baseFileName=vtkFilename)
+    
+    writer.add_nodal_field("bcs", nodalData=np.array(essentialBCManager.isBc, dtype=int),
+                           fieldType=VTKWriter.VTKFieldType.VECTORS, dataType=VTKWriter.VTKDataType.INT)
+    
+    writer.add_nodal_field("displacement", nodalData=U, fieldType=VTKWriter.VTKFieldType.VECTORS)
+
+    internalVariables = p[1]
+    energyDensities, stresses = solidMechanics.compute_output_energy_densities_and_stresses(
+        U, internalVariables)
+    cellEnergyDensities = FunctionSpace.project_quadrature_field_to_element_field(
+        fs, energyDensities)
+    cellStresses = FunctionSpace.project_quadrature_field_to_element_field(
+        fs, stresses)
+    
+    writer.add_cell_field("strain_energy_density", cellEnergyDensities, VTKWriter.VTKFieldType.SCALARS)
+    writer.add_cell_field("stress", cellStresses, VTKWriter.VTKFieldType.TENSORS)
+    writer.write()
+
+
+solverSettings = EquationSolver.get_settings(max_cumulative_cg_iters=100,
+                                             max_trust_iters=100,
+                                             use_preconditioned_inner_product_for_cg=True)
+
+precondStrategy = Objective.PrecondStrategy(assemble_sparse_preconditioner_matrix)
+
+U = np.zeros_like(mesh.coords)
+Uu = essentialBCManager.get_unknown_values(U)
+appliedK = 0.0
+state = solidMechanics.compute_initial_state()
+
+p = Objective.Params(appliedK, state)
+objective = Objective.Objective(potential_energy, Uu, p, precondStrategy)
+
+for i in range(1):
+    appliedK += 1.0
+    p = Objective.param_index_update(p, 0, appliedK)
+
+    Uu = EquationSolver.nonlinear_equation_solve(objective, Uu, p, solverSettings)
+
+    U = create_field(Uu, p)
+    write_output(U, p, i)
+
