779b74cf99d3799939db75af05f65ddad55977a4
Brandon Talamini <talamini1@llnl.gov>
2022-11-22 14:01:52 -0800

    Remove unneccessary infinities for negative EQPS in hardening functions
    We have a solver that has a lower bound on the EQPS increment,
    (which is set to 0) so we're already protected against invalid
    values. When the hardening functions return infinity on negative
    EQPS, this can get triggered during iterations due to floating
    point cancellation or during an iterate that is going to be
    safely changed by the solver, which is counterproductive.
    
    Also, the only hardening function with a singular point (power law)
    will automatically return a NaN or inf when the singularity is hit,
    so there's no need to put an external condition.
    
    Also in this commit:
    * Remove wildcard import
    * Fix incorrect exception type name from standard library (should
      be "ValueError", not "valueError"

--- optimism/material/Hardening.py
+++ optimism/material/Hardening.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
-from optimism.JaxConfig import *
+
+import jax
+import jax.numpy as np
 
 HardeningModel = namedtuple('HardeningModel', ['compute_hardening_energy_density', 'compute_flow_stress'])
 
@@ -28,26 +30,20 @@ def create_hardening_model(properties):
             return power_law(eqps, Y0, n, eps0)
 
     else:
-        raise valueError('Unknown hardening model specified')
+        raise ValueError('Unknown hardening model specified')
 
-    return HardeningModel(hardening_energy_density, grad(hardening_energy_density))
+    return HardeningModel(hardening_energy_density, jax.grad(hardening_energy_density))
 
 
 def linear(eqps, Y0, H):
-    return np.where(eqps >= 0.0,
-                    Y0*eqps + 0.5*H*eqps**2,
-                    np.inf)
+    return Y0*eqps + 0.5*H*eqps**2
 
 
 def voce(eqps, Y0, Ysat, eps0):
-    return np.where(eqps >= 0.0,
-                    Ysat*eqps + (Ysat - Y0)*eps0*(np.expm1(-eqps/eps0)),
-                    np.inf)
+    return Ysat*eqps + (Ysat - Y0)*eps0*(np.expm1(-eqps/eps0))
 
 
 def power_law(eqps, Y0, n, eps0):
     A = n*Y0*eps0/(1.0 + n)
     x = eqps/eps0
-    return np.where(x >= 0.0,
-                    A*( (1.0 + x)**((n+1)/n) - 1.0 ),
-                    np.inf)
+    return A*( (1.0 + x)**((n+1)/n) - 1.0 )
