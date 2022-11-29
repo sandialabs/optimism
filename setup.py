14ea79a1a0a075b3fd9d8c6082283798a68e8cf2
Craig M. Hamel <chamel@sandia.gov>
2022-11-29 13:37:35 -0700

    first moving things to a more 'modern' python 'build system' which should be easier to maintain things like version number, dependencies, optional build dependencies, and author lists etc. rather than changing setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name='optimism',
-    description='Rapid development platform for solid mechanics research using optimization tools',
-    author="Michael Tupek and Brandon Talamini",
-    author_email='talamini1@llnl.gov', # todo: make an email list
-    install_requires=['jax',
-                      'jaxlib',
-                      'scipy',
-                      'matplotlib', # this is not strictly necessary
-                      'netcdf4'],
-    #tests_require=[], # could put chex and pytest here
-    extras_require={'sparse': ['scikit-sparse']},
-    python_requires='>=3.7',
-    version='0.0.1',
-    license='MIT',
-    url='https://github.com/sandialabs/optimism'
-)
+
+if __name__ == '__main__':
+    setup()
