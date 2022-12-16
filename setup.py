60b9d560f54813729c3b474f6db2338695d50089
Craig M. Hamel <chamel@sandia.gov>
2022-12-16 12:40:27 -0700

    adding another extra in setup.py for testing. Adds pytest, pytest-cov, and pytest-xdist as optional dependencies. Pytest-cov can give us coverage reporting (although we may need to reorganize test locations so pytest-cov doesn't cover tests). Pytest-xdist allows for parallel unit testing which can speed up the develop/test cycle and speed up runners as well for CI.

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setuptools.setup(
                       'matplotlib', # this is not strictly necessary
                       'netcdf4'],
     #tests_require=[], # could put chex and pytest here
-    extras_require={'sparse': ['scikit-sparse']},
+    extras_require={'sparse': ['scikit-sparse'],
+                    'test': ['pytest', 'pytest-cov', 'pytest-xdist']},
     python_requires='>=3.7',
     version='0.0.1',
     license='MIT',
