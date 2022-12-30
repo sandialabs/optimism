fc646fc24215b6d788355ac48212dbae406ab84a
Brandon Talamini <brandon.talamini@gmail.com>
2022-12-30 01:25:33 -0800

    Update more pahse field tests

--- optimism/phasefield/test/testPhaseFieldThreshold.py
+++ optimism/phasefield/test/testPhaseFieldThreshold.py
@@ -31,6 +31,7 @@ class PhaseFieldThresholdModelFixture(TestFixture.TestFixture):
         self.model = Model.create_material_model_functions(props)
         self.flux_func = grad(self.model.compute_energy_density, (0,1,2))
         self.internalVariables = self.model.compute_initial_state()
+        self.dt = 0.0 # unused - material has no rate dependence
 
 
     def test_zero_point(self):
@@ -38,10 +39,10 @@ class PhaseFieldThresholdModelFixture(TestFixture.TestFixture):
         phase = 0.
         phaseGrad = np.zeros(3)
 
-        energy = self.model.compute_energy_density(dispGrad, phase, phaseGrad, self.internalVariables)
+        energy = self.model.compute_energy_density(dispGrad, phase, phaseGrad, self.internalVariables, self.dt)
         self.assertNear(energy, 0.0, 12)
 
-        stress, phaseForce, phaseGradForce = self.flux_func(dispGrad, phase, phaseGrad, self.internalVariables)
+        stress, phaseForce, phaseGradForce = self.flux_func(dispGrad, phase, phaseGrad, self.internalVariables, self.dt)
         self.assertArrayNear(stress, np.zeros((3,3)), 12)
         self.assertNear(phaseForce, 3.0/8.0*self.Gc/self.l, 12)
         self.assertArrayNear(phaseGradForce, np.zeros(3), 12)
@@ -54,14 +55,15 @@ class PhaseFieldThresholdModelFixture(TestFixture.TestFixture):
         phase = random.uniform(subkey)
         key,subkey = random.split(key)
         phaseGrad = random.uniform(subkey, (3,))
-        energy = self.model.compute_energy_density(dispGrad, phase, phaseGrad, self.internalVariables)
+        dt = 0.0
+        energy = self.model.compute_energy_density(dispGrad, phase, phaseGrad, self.internalVariables, self.dt)
         
         Q = R.random(random_state=1234).as_matrix()
         dispGradStar = Q@(dispGrad + np.identity(3)) - np.identity(3)
         phaseStar = phase
         phaseGradStar = Q@phaseGrad
         internalVariablesStar = self.internalVariables
-        energyStar = self.model.compute_energy_density(dispGradStar, phaseStar, phaseGradStar, internalVariablesStar)
+        energyStar = self.model.compute_energy_density(dispGradStar, phaseStar, phaseGradStar, internalVariablesStar, self.dt)
         self.assertNear(energy, energyStar, 12)
 
 
@@ -72,13 +74,13 @@ class PhaseFieldThresholdModelFixture(TestFixture.TestFixture):
         phase = 0.15
         phaseGrad = np.zeros(3)
 
-        energy = self.model.compute_energy_density(dispGrad, phase, phaseGrad, self.internalVariables)
+        energy = self.model.compute_energy_density(dispGrad, phase, phaseGrad, self.internalVariables, self.dt)
 
         g = (1.0 - phase)**2
         energyExact = g*0.5*self.E*strain**2 + 3/8*self.Gc/self.l*phase
         self.assertNear(energy, energyExact, 12)
 
-        piolaStress,_,_ = self.flux_func(dispGrad, phase, phaseGrad, self.internalVariables)
+        piolaStress,_,_ = self.flux_func(dispGrad, phase, phaseGrad, self.internalVariables, self.dt)
         kStress = piolaStress@(dispGrad + np.identity(3)).T
         kStressExact = np.zeros((3,3)).at[0,0].set(g*self.E*strain)
         self.assertArrayNear(kStress, kStressExact, 12)
