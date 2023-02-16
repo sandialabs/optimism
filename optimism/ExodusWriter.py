8837245dde45ec1c182f1feb9cc9606ce996584d
Craig M. Hamel <chamel@sandia.gov>
2023-02-16 07:58:53 -0700

    adding in a simple ExodusWriter. Not quite elegant yet, but it makes things easy for us on the Sandia end.

--- optimism/ExodusWriter.py
+++ optimism/ExodusWriter.py
@@ -1,4 +1,6 @@
 import exodus3 as exodus
+import jax.numpy as np
+import numpy as onp
 import os
 from typing import List
 
@@ -18,7 +20,12 @@ def setup_exodus_database(exo_file: str,
                           num_node_variables: int, 
                           num_element_variables: int,
                           node_variable_names: List[str], 
-                          element_variable_names: List[str]) -> None:
+                          element_variable_names: List[str]) -> exodus.exodus:
+
+    assert num_node_variables == len(node_variable_names), \
+        'Number of node variables needs to match the number of node variable names!'
+    assert num_element_variables == len(element_variable_names), \
+        'Number of element variables needs to match the number of element variable names!'
 
     exo = exodus.exodus(exo_file, 'a', array_type='numpy')
     exo.set_node_variable_number(num_node_variables)
@@ -30,8 +37,27 @@ def setup_exodus_database(exo_file: str,
     for n in range(len(element_variable_names)):
         exo.put_element_variable_name(element_variable_names[n], n + 1)
 
-    exo.close()
+    return exo
 
 
-def write_nodal_variables():
-    pass
+def write_exodus_nodal_outputs(exo: exodus.exodus,
+                               node_variable_names: List[str],
+                               node_variable_values: List[str],
+                               time_step: int) -> None:
+    
+    assert len(node_variable_names) == len(node_variable_values), \
+        'Number of node variable names needs to match the number of node variable value arrays!'
+    
+    for n in range(len(node_variable_names)):
+        exo.put_node_variable_values(node_variable_names[n], time_step, onp.array(node_variable_values[n]))
+
+    
+def write_exodus_element_outputs(exo: exodus.exodus,
+                                 element_variable_names: List[np.ndarray],
+                                 element_variable_values: List[np.ndarray],
+                                 time_step: int, block_id: int) -> None:
+    assert len(element_variable_names) == len(element_variable_values), \
+        'Number of element variable names needs to match the number of element variable value arrays!'
+
+    for n in range(len(element_variable_names)):
+        exo.put_element_variable_values(block_id, element_variable_names[n], time_step, onp.array(element_variable_values[n]))
