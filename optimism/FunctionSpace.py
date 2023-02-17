3ba87ecdf904dd8a0f784b57035bd2f2ff410f46
Craig M. Hamel <chamel@sandia.gov>
2023-02-17 12:31:34 -0700

    adding the following 1. A carbon copy of the create function space method that exposes nodal coordinates for calculating sensitivities. 2. Adding an example called 'plato_inteface' to show how to run optimism and get sensities of a simple objective function.
    Note that we can't just AD through the solver since things get converted from jax.numpy to numpy
    
    Need to scope the state of jax's scipy implementation

--- optimism/FunctionSpace.py
+++ optimism/FunctionSpace.py
@@ -34,6 +34,28 @@ FunctionSpace.__doc__ = \
 EssentialBC = namedtuple('EssentialBC', ['nodeSet', 'component'])
 
 
+# for AD of nodal coordinates purposes
+def construct_function_space_for_adjoint(coords, mesh, quadratureRule, mode2D='cartesian'):
+    shapeOnRef = Interpolants.compute_shapes(mesh.parentElement, quadratureRule.xigauss)
+
+    shapes = jax.vmap(lambda elConns, elShape: elShape, (0, None))(mesh.conns, shapeOnRef.values)
+
+    shapeGrads = jax.vmap(map_element_shape_grads, (None, 0, None, None))(coords, mesh.conns, mesh.parentElement, shapeOnRef.gradients)
+
+    if mode2D == 'cartesian':
+        el_vols = compute_element_volumes
+    elif mode2D == 'axisymmetric':
+        el_vols = compute_element_volumes_axisymmetric
+    vols = jax.vmap(el_vols, (None, 0, None, 0, None))(coords, mesh.conns, mesh.parentElement, shapes, quadratureRule.wgauss)
+
+    # unpack mesh and remake a mesh to make sure we get all the AD
+    mesh = Mesh.Mesh(coords=coords, conns=mesh.conns, simplexNodesOrdinals=mesh.simplexNodesOrdinals,
+                     parentElement=mesh.parentElement, parentElement1d=mesh.parentElement1d, blocks=mesh.blocks,
+                     nodeSets=mesh.nodeSets, sideSets=mesh.sideSets)
+
+    return FunctionSpace(shapes, vols, shapeGrads, mesh, quadratureRule)
+
+
 def construct_function_space(mesh, quadratureRule, mode2D='cartesian'):
     """Construct a discrete function space.
 
