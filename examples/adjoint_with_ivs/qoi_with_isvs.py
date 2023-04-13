03a5340a341d295fd25ec1231b4e58e6503ee5fb
Brandon Talamini <talamini1@llnl.gov>
2023-04-13 14:29:27 -0700

    Bugfix: add in missing terms to adjoint internal variable force

--- examples/adjoint_with_ivs/qoi_with_isvs.py
+++ examples/adjoint_with_ivs/qoi_with_isvs.py
@@ -29,12 +29,10 @@ steps = 50
 t_max = max_strain/strain_rate
 dt = t_max/steps
 
-def make_strain(axial_strain, lateral_strain):
-    return np.diag(np.array([axial_strain, lateral_strain, lateral_strain]))
 
 def f(lateral_strain, axial_strain, state, E, nu, Y0, H):
     """Compute residual for uniaxial stress state."""
-    strain = make_strain(axial_strain, lateral_strain)
+    strain = np.diag(np.array([axial_strain, lateral_strain, lateral_strain]))
     stress = compute_stress(strain, state, dt, E, nu, Y0, H)
     return stress[1,1]
 
@@ -50,7 +48,6 @@ def qoi(strain_new, isv_new, isv_old, E, nu, Y0, H):
 
 t = 0.0
 strain = np.zeros((3,3))
-strain_old = np.zeros((3,3))
 internal_state = material.compute_initial_state()
 D = 0.0
 stress_history = onp.zeros((steps, 3, 3))
@@ -68,6 +65,7 @@ for i in range(1, steps):
 
     lat_strain = solve(strain[1,1], strain[0,0], internal_state, E, nu, Y0, H)
 
+    # post-process and save data needed for reverse pass
     strain = np.diag(np.array([strain[0,0], lat_strain, lat_strain]))
     stress = compute_stress(strain, internal_state, dt, E, nu, Y0, H)
     internal_state_old = internal_state
@@ -88,11 +86,12 @@ compute_energy_density(strain_history[-1, :, :], internal_state, dt, E, nu, Y0, 
 print(f"QOI: {D:6e}")
 print(f"Exact:    {D_exact:6e}")
 
-print(f"strain hist {strain_history[:, 0, 0]}")
-print(f"stress hist {stress_history[:, 0, 0]}")
-
+# print(f"strain hist {strain_history[:, 0, 0]}")
+# print(f"stress hist {stress_history[:, 0, 0]}")
 # plt.plot(strain_history[:, 0, 0], stress_history[:, 0, 0])
 # plt.show()
+
+
 
 # Phase 2:
 # compute sensitivity
@@ -100,9 +99,9 @@ print(f"stress hist {stress_history[:, 0, 0]}")
 compute_adjoint_load = jax.jit(jax.grad(qoi, 0))
 compute_pseudo_load = jax.jit(jax.grad(f, 5))
 compute_explicit_sensitivity = jax.jit(jax.grad(qoi, (5,)))
-
+compute_dpi_dq = jax.jit(jax.grad(qoi, (1,2)))
 compute_dq = jax.jit(jax.jacrev(compute_state_new, (0, 1, 5)))
-compute_foo = jax.jit(jax.jacfwd(f, 2))
+compute_dstress_dq = jax.jit(jax.jacfwd(f, 2))
 
 qoi_derivatives = 0.0
 adjoint_internal_state_force = np.zeros_like(internal_state)
@@ -121,6 +120,8 @@ for i in reversed(range(1, steps)):
     K = df(strain[1,1], strain[0,0], internal_state_old, E, nu, Y0, H)
     F = -compute_adjoint_load(strain, internal_state, internal_state_old, E, nu, Y0, H)[1,1]
     dQ_dStrain, dQ_dQOld, dQ_Y0 = compute_dq(strain, internal_state_old, dt, E, nu, Y0, H)
+    dpi_dQ, dpi_dQold = compute_dpi_dq(strain, internal_state, internal_state_old, E, nu, Y0, H)
+    adjoint_internal_state_force += dpi_dQ
     F -= np.tensordot(adjoint_internal_state_force, dQ_dStrain, axes=1)[1,1]
     W = F/K
 
@@ -131,7 +132,8 @@ for i in reversed(range(1, steps)):
     qoi_derivatives += np.dot(adjoint_internal_state_force, dQ_Y0)
 
     adjoint_internal_state_force = np.tensordot(adjoint_internal_state_force, dQ_dQOld, axes=1)
-    adjoint_internal_state_force += W*compute_foo(strain[1,1], strain[0,0], internal_state_old, E, nu, Y0, H)
+    adjoint_internal_state_force += W*compute_dstress_dq(strain[1,1], strain[0,0], internal_state_old, E, nu, Y0, H)
+    adjoint_internal_state_force += dpi_dQold
     
     # print(f"strain={strain}")
     # print(f"strain_old={strain_old}")
