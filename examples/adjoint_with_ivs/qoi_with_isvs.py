72da161bdd0dbb498dd283cfb2aed16de4516ee3
Brandon Talamini <talamini1@llnl.gov>
2023-04-18 13:29:40 -0700

    Get backward pass running
    Results need to be checked

--- examples/adjoint_with_ivs/qoi_with_isvs.py
+++ examples/adjoint_with_ivs/qoi_with_isvs.py
@@ -13,23 +13,23 @@ def simulate(material, max_strain, strain_rate, steps, compute_qoi, *params):
     pi, _ = simulate_fwd(material, max_strain, strain_rate, steps, compute_qoi, *params)
     return pi
 
+
 def simulate_fwd(material, max_strain, strain_rate, steps, compute_qoi, *params):
     t_max = max_strain/strain_rate
     dt = t_max/steps
     compute_stress = jax.jit(jax.grad(material.compute_energy_density))
-    #compute_tangents = jax.jit(jax.jacfwd(compute_stress))
+    compute_tangents = jax.jit(jax.jacfwd(compute_stress))
     compute_state_new = jax.jit(material.compute_state_new)
     #compute_energy_density = jax.jit(material.compute_energy_density)
 
-
-    def f(lateral_strain, axial_strain, state, *params):
+    def r(lateral_strain, axial_strain, state, dt, *params):
         """Compute residual for uniaxial stress state."""
         strain = np.diag(np.array([axial_strain, lateral_strain, lateral_strain]))
         stress = compute_stress(strain, state, dt, *params)
         return stress[1,1]
 
-    df = jax.jacfwd(f)
-    solve = jax.jit(partial(nonlinear.solve, f, df))
+    dr = jax.jacfwd(r)
+    solve = jax.jit(partial(nonlinear.solve, r, dr))
 
     t = 0.0
     strain = np.zeros((3,3))
@@ -49,7 +49,7 @@ def simulate_fwd(material, max_strain, strain_rate, steps, compute_qoi, *params)
         strain = strain.at[0,0].add(strain_rate*dt)
         t += dt
 
-        lat_strain = solve(strain[1,1], strain[0,0], internal_state, *params)
+        lat_strain = solve(strain[1,1], strain[0,0], internal_state, dt, *params)
 
         # post-process and save data needed for reverse pass
         strain = np.diag(np.array([strain[0,0], lat_strain, lat_strain]))
@@ -64,26 +64,32 @@ def simulate_fwd(material, max_strain, strain_rate, steps, compute_qoi, *params)
 
         print(f"residual norm: {np.abs(stress[1,1])}")
         print(f"strain = {strain[0,0]}, \tstress = {stress[0,0]}")
+        print(f"internal={internal_state}")
         print(f"QOI: {pi:6e}")
         
         history = strain_history, stress_history, internal_state_history, time_history
-        helper_functions = compute_stress, compute_state_new, df
 
-    return pi, (history, helper_functions, params)
+    return pi, (history, params)
 
 
-def simulate_bwd(material, compute_qoi, tape, cotangents):
-    history, helper_functions, params = tape
+def simulate_bwd(material, compute_qoi, tape, cotangent):
+    history, params = tape
     strain_history, stress_history, internal_state_history, time_history = history
-    compute_stress, compute_state_new, df = helper_functions
-    g_pi = cotangents
+    compute_stress = jax.jit(jax.grad(material.compute_energy_density))
+    compute_state_new = jax.jit(material.compute_state_new)
+    def r(lateral_strain, axial_strain, state, dt, *params):
+        """Compute residual for uniaxial stress state."""
+        strain = np.diag(np.array([axial_strain, lateral_strain, lateral_strain]))
+        stress = compute_stress(strain, state, dt, *params)
+        return stress[1,1]
+    dr = jax.jit(jax.jacfwd(r))
 
     compute_dpi = jax.jit(jax.grad(compute_qoi, (0, 1, 2, 4)))
     compute_dq = jax.jit(jax.jacrev(compute_state_new, (0, 1, 3)))
     compute_dsigma = jax.jit(jax.jacrev(compute_stress, (0, 1, 3)))
     
-    qoi_derivatives = 0.0
-    adjoint_internal_state_force = np.zeros_like(internal_state)
+    qoi_derivatives = np.zeros_like(*params)
+    adjoint_internal_state_force = np.zeros_like(internal_state_history[0])
     F = 0.0 # adjoint load
     print("\n\n")
     print("ADJOINT PHASE")
@@ -97,17 +103,17 @@ def simulate_bwd(material, compute_qoi, tape, cotangents):
         internal_state_old = internal_state_history[i-1]
         dt = time_history[i] - time_history[i-1]
         
-        dpi_dH, dpi_dQ, dpi_dQOld, dpi_dp = compute_dpi(strain, internal_state, internal_state_old, params)
-        dQ_dH, dQ_dQOld, dQ_dp = compute_dq(strain, internal_state_old, dt, params)
-        K = df(strain[1,1], strain[0,0], internal_state_old, params)
+        dpi_dH, dpi_dQ, dpi_dQOld, dpi_dp = compute_dpi(strain, internal_state, internal_state_old, dt, *params)
+        dQ_dH, dQ_dQOld, dQ_dp = compute_dq(strain, internal_state_old, dt, *params)
+        K = dr(strain[1,1], strain[0,0], internal_state_old, dt, *params)
         F = -dpi_dH[1,1]
         adjoint_internal_state_force += dpi_dQ
         F -= np.tensordot(adjoint_internal_state_force, dQ_dH, axes=1)[1,1]
         W = F/K
 
         qoi_derivatives += dpi_dp
-        dSigma_dH, dSigma_dQOld, dSigma_dY0 = compute_dsigma(strain, internal_state_old, dt, params)
-        qoi_derivatives += W*dSigma_dY0[1,1]
+        dSigma_dH, dSigma_dQOld, dSigma_dp = compute_dsigma(strain, internal_state_old, dt, *params)
+        qoi_derivatives += W*dSigma_dp[1,1]
         qoi_derivatives += np.dot(adjoint_internal_state_force, dQ_dp)
 
         adjoint_internal_state_force = np.tensordot(adjoint_internal_state_force, dQ_dQOld, axes=1)
@@ -118,9 +124,10 @@ def simulate_bwd(material, compute_qoi, tape, cotangents):
         # print(f"strain_old={strain_old}")
         # print(f"stress = {stress}")
         print(f"W = {W}, F = {F}")
+        print(f"adjoint internal var force = {adjoint_internal_state_force}")
         print(f"dqoi={qoi_derivatives}")
         
-        return 0.0, 0.0, 0, (0.0, 0.0, cotangents*qoi_derivatives, 0.0)
+    return 0.0, 0.0, 0, cotangent*qoi_derivatives
 
 simulate.defvjp(simulate_fwd, simulate_bwd)
 
@@ -146,10 +153,7 @@ if __name__ == "__main__":
         plastic_strain_old = isv_old[Material.STATE_PLASTIC_STRAIN].reshape(3,3)
         return np.tensordot(stress_new, plastic_strain_new - plastic_strain_old)
 
-    #output, pi = simulate(material, max_strain, strain_rate, steps, compute_qoi, E, nu, Y0, H)
-    #F = jax.grad(simulate, (5,))
-    #dpi = F(material, max_strain, strain_rate, steps, compute_qoi, params)
-    pi = simulate(material, max_strain, strain_rate, steps, compute_qoi, params)
+    pi, dpi_dp = jax.value_and_grad(simulate, 5)(material, max_strain, strain_rate, steps, compute_qoi, params)
 
     eqps = (E*max_strain - Y0)/(E + H)
     pi_exact = Y0*eqps + 0.5*H*eqps**2
@@ -163,7 +167,11 @@ if __name__ == "__main__":
     # plt.plot(strain_history[:, 0, 0], stress_history[:, 0, 0])
     # plt.show()
     
-    #eqps = internal_state_history[-1, Material.EQPS]
-    # dD_dY0_exact = eqps - (Y0+H*eqps)/(H+E)
-    # print(f"dqoi_dY0 = {dpi[2]:6e}")
-    # print(f"exact   = {dD_dY0_exact:6e}")
+    print(f"dpi_dE = {dpi_dp[0]:6e}")
+    print(f"exact  = {(Y0+H*eqps)*(H*max_strain+Y0)/(H+E)**2:6e}")
+    dD_dY0_exact = eqps - (Y0+H*eqps)/(H+E)
+    print(f"dpi_dY0  = {dpi_dp[2]:6e}")
+    print(f"exact    = {dD_dY0_exact:6e}")
+    dpi_dH_exact = -(Y0+H*eqps)*(E*max_strain-Y0)/(H+E)**2
+    print(f"dpi_dH = {dpi_dp[3]:6e}")
+    print(f"exact = {dpi_dH_exact:6e}")
