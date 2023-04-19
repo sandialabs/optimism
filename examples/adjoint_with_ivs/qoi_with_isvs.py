a82efeb3a549d872d7a7cca5ddfae04f7f3d5841
Brandon Talamini <talamini1@llnl.gov>
2023-04-19 15:28:49 -0700

    Implement command line option parsing for qoi type

--- examples/adjoint_with_ivs/qoi_with_isvs.py
+++ examples/adjoint_with_ivs/qoi_with_isvs.py
@@ -1,3 +1,6 @@
+"""Compute one of several example quantitities of interest for a material with state."""
+import argparse
+
 import jax
 from jax import numpy as np
 import matplotlib.pyplot as plt
@@ -28,13 +31,28 @@ def compute_plastic_work(strain_new, strain_old, isv_new, isv_old, dt, params):
     plastic_strain_old = isv_old[Material.STATE_PLASTIC_STRAIN].reshape(3,3)
     return np.tensordot(stress_new, plastic_strain_new - plastic_strain_old)
 
-pi, (history, _) = material_point.simulate_fwd(material, max_strain, strain_rate, steps, compute_plastic_work, params)
-strain_history = history[0]
-stress_history = history[1]
-plt.plot(strain_history[:, 0, 0], stress_history[:, 0, 0])
-plt.show()
+def compute_lateral_strain(strain_new, strain_old, isv_new, isv_old, dt, params):
+    return strain_new[1,1] - strain_old[1,1]
 
-pi, dpi_dp = jax.value_and_grad(material_point.simulate, 5)(material, max_strain, strain_rate, steps, compute_plastic_work, params)
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("--qoi",
+                    choices=["plasticwork", "lateralstrain"],
+                    default="plasticwork")
+args = parser.parse_args()
+
+print(f"QoI is {args.qoi}")
+if args.qoi == "plasticwork":
+    compute_qoi = compute_plastic_work
+elif args.qoi == "lateralstrain":
+    compute_qoi = compute_lateral_strain
+
+# pi, (history, _) = material_point.simulate_fwd(material, max_strain, strain_rate, steps, compute_lateral_strain, params)
+# strain_history = history[0]
+# stress_history = history[1]
+# plt.plot(strain_history[:, 0, 0], stress_history[:, 0, 0])
+# plt.show()
+
+pi, dpi_dp = jax.value_and_grad(material_point.simulate, 5)(material, max_strain, strain_rate, steps, compute_qoi, params)
 
 eqps = (E*max_strain - Y0)/(E + H)
 pi_exact = Y0*eqps + 0.5*H*eqps**2
@@ -65,3 +83,15 @@ dpi_dH_exact = 0.5*eqps**2-H*eqps*(E*max_strain-Y0)/(H+E)**2
 print("")
 print(f"dpi_dH = {dpi_dp[3]:6e}")
 print(f"exact = {dpi_dH_exact:6e}")
+
+# dpi_dp_h = np.zeros_like(params)
+# for i in range(params.shape[0]):
+#     h = 1e-5*params[i]
+#     params_p = params.at[i].add(h)
+#     pi_p = material_point.simulate(material, max_strain, strain_rate, steps, compute_qoi, params_p)
+#     params_m = params.at[i].add(-h)
+#     pi_m = material_point.simulate(material, max_strain, strain_rate, steps, compute_qoi, params_m)
+#     dpi_dp_h = dpi_dp_h.at[i].set((pi_p - pi_m)/(2.0*h))
+
+# print(f"dpi_dp   = {dpi_dp}")
+# print(f"dpi_dp_h = {dpi_dp_h}")
