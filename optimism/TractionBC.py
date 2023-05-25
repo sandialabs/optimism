d50e42cafe229b6f13d9af7ad501a1f55c4ace57
Brandon Talamini <talamini1@llnl.gov>
2023-05-25 15:54:19 -0700

    Re-implement traction computations to work with higher order elements and test it

--- optimism/TractionBC.py
+++ optimism/TractionBC.py
@@ -1,25 +1,39 @@
 import jax
 import jax.numpy as np
 
-from optimism import QuadratureRule
-from optimism import Surface
+from optimism import Interpolants
 
 
-def interpolate_nodal_field_on_edge(mesh, U, quadRule, edge):
-    fieldIndex = Surface.get_field_index(edge, mesh.conns)  
-    nodalValues = Surface.eval_field(U, fieldIndex)    
-    return QuadratureRule.eval_at_iso_points(quadRule.xigauss, nodalValues)
+def interpolate_nodal_field_on_edge(functionSpace, U, quadRule, edge):
+    elemOrdinal = edge[0]
+    sideOrdinal = edge[1]
+    elemNodeOrdinals = functionSpace.mesh.parentElement.faceNodes[sideOrdinal, :]
+    fieldIndex = functionSpace.mesh.conns[elemOrdinal, elemNodeOrdinals]
+    edgeShapes = Interpolants.compute_shapes(functionSpace.mesh.parentElement1d, quadRule.xigauss)
+    edgeU = U[fieldIndex, :]
+    return edgeShapes.values.T@edgeU
 
+def get_edge_coords(mesh, edge):
+    edgeNodes = mesh.parentElement.faceNodes[edge[1], :]
+    nodes = mesh.conns[edge[0], edgeNodes]
+    return mesh.coords[nodes, :]
 
-def compute_traction_potential_energy_on_edge(mesh, U, quadRule, edge, load, time):
-    uq = interpolate_nodal_field_on_edge(mesh, U, quadRule, edge)
-    Xq = interpolate_nodal_field_on_edge(mesh, mesh.coords, quadRule, edge)
-    edgeCoords = Surface.get_coords(mesh, edge)
-    edgeNormal = Surface.compute_normal(edgeCoords)
-    tq = jax.vmap(load, (0, None, None))(Xq, edgeNormal, time)
+def compute_edge_vectors(mesh, edgeCoords):
+    Xv = edgeCoords[mesh.parentElement1d.vertexNodes, :]
+    tangent = Xv[1] - Xv[0]
+    normal = np.array([tangent[1], -tangent[0]])
+    jac = np.linalg.norm(tangent)
+    return tangent/jac, normal/jac, jac
+
+def compute_traction_potential_energy_on_edge(fs, U, quadRule, edge, load, time):
+    uq = interpolate_nodal_field_on_edge(fs, U, quadRule, edge)
+    Xq = interpolate_nodal_field_on_edge(fs, fs.mesh.coords, quadRule, edge)
+    edgeCoords = get_edge_coords(fs.mesh, edge)
+    _, normal, jac = compute_edge_vectors(fs.mesh, edgeCoords)
+    tq = jax.vmap(load, (0, None, None))(Xq, normal, time)
     integrand = jax.vmap(lambda u,t: u@t)(uq, tq)
-    return -Surface.integrate_values(quadRule, edgeCoords, integrand)
+    return -np.dot(integrand, jac*quadRule.wgauss)
 
 
-def compute_traction_potential_energy(mesh, U, quadRule, edges, load, time=0.0):
-    return np.sum( jax.vmap(compute_traction_potential_energy_on_edge, (None,None,None,0,None,None))(mesh, U, quadRule, edges, load, time) )
+def compute_traction_potential_energy(fs, U, quadRule, edges, load, time=0.0):
+    return np.sum( jax.vmap(compute_traction_potential_energy_on_edge, (None,None,None,0,None,None))(fs, U, quadRule, edges, load, time) )
