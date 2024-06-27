42fe36755f41cb0412cdb5843b6de01f7d5f2a8e
Craig M. Hamel <chamel@sandia.gov>
2024-06-27 11:58:33 -0600

    Changed version number in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     extras_require={'sparse': ['scikit-sparse'],
                     'test': ['pytest', 'pytest-cov', 'pytest-xdist']},
     python_requires='>=3.7',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     url='https://github.com/sandialabs/optimism'
 )
