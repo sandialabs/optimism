920030f9cf8bbbf95c244f0a95c039f4e0749e20
Craig M. Hamel <chamel@sandia.gov>
2025-01-08 09:57:43 -0700

    removing scipy upperbound.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
                       'matplotlib', # this is not strictly necessary
                       'metis',
                       'netcdf4',
-                      'scipy<1.15.0',],
+                      'scipy',],
     #tests_require=[], # could put chex and pytest here
     extras_require={'sparse': ['scikit-sparse'],
                     'test': ['pytest', 'pytest-cov', 'pytest-xdist'],
