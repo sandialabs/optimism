concatenates shape gradients and hessians into same container in order to facilitate transfer to material points. Requires ensuring material points only take correct components for displacement gradients. Requires changes to element modification functions (only updated plane strain)

optimism/FunctionSpace.py
@@ -22,7 +22,6 @@ class FunctionSpace(eqx.Module):
     elements in the mesh, ``nqpe`` is the number of quadrature points per
     element, ``npe`` is the number of nodes per element, and ``nd`` is the
     spatial dimension of the domain.
-    For shape Hessians, nh is the number of hessian entries, set to 3
 
     Attributes:
         shapes: Shape function values on each element, shape (ne, nqpe, npe)
@@ -32,9 +31,6 @@ class FunctionSpace(eqx.Module):
             element in the domain. Shape (ne, nqpe).
         shapeGrads: Derivatives of the shape functions with respect to the
             spatial coordinates of the domain. Shape (ne, nqpe, npe, nd).
-        shapeHessians: Second derivatives of the shape functions with respect to the
-            spatial coordinates of the domain. Shape (ne, nqpe, npe, nh).
-            These are only implemented for quadratic Lagrange elements.
         mesh: The ``Mesh`` object of the domain.
         quadratureRule: The ``QuadratureRule`` on which to sample the shape
             functions.
@@ -44,7 +40,6 @@ class FunctionSpace(eqx.Module):
     shapes: Float[Array, "ne nqpe nn"]
     vols: Float[Array, "ne nqpe"]
     shapeGrads: Float[Array, "ne nqpe nn nd"]
-    shapeHessians: Float[Array, "ne nqpe nn nh"]
     mesh: Mesh.Mesh
     quadratureRule: QuadratureRule.QuadratureRule
     isAxisymmetric: bool
@@ -120,9 +115,9 @@ def construct_function_space_from_parent_element(mesh, shapeOnRef, quadratureRul
     if mesh.parentElement.elementType == Interpolants.LAGRANGE_TRIANGLE_ELEMENT:
         shapeOnRefHessians = Interpolants.shape2d_lagrange_second_derivatives(mesh.parentElement.degree, mesh.parentElement.coordinates, quadratureRule.xigauss)
         shapeHessians = jax.vmap(map_element_shape_hessians, (None, 0, None, None, None))(mesh.coords, mesh.conns, mesh.parentElement, shapeOnRef.gradients, shapeOnRefHessians)
-        return FunctionSpace(shapes, vols, shapeGrads, shapeHessians, mesh, quadratureRule, isAxisymmetric)
+        return FunctionSpace(shapes, vols, np.concatenate((shapeGrads, shapeHessians), axis=-1), mesh, quadratureRule, isAxisymmetric)
     else:
-        return FunctionSpace(shapes, vols, shapeGrads, None, mesh, quadratureRule, isAxisymmetric)
+        return FunctionSpace(shapes, vols, shapeGrads, mesh, quadratureRule, isAxisymmetric)
 
 
 def map_element_shape_grads(coordField, nodeOrdinals, parentElement, shapeGradients):
@@ -218,12 +213,6 @@ def compute_field_gradient(functionSpace, nodalField, modify_element_gradient=de
     return jax.vmap(compute_element_field_gradient, (None,None,0,0,0,0,None))(nodalField, functionSpace.mesh.coords, functionSpace.shapes, functionSpace.shapeGrads, functionSpace.vols, functionSpace.mesh.conns, modify_element_gradient)
 
 
-def compute_field_hessian(functionSpace, nodalField):
-    if functionSpace.shapeHessians == None:
-        raise NotImplementedError
-    return jax.vmap(compute_element_field_hessian, (None,0,0))(nodalField, functionSpace.shapeHessians, functionSpace.mesh.conns)
-
-
 def interpolate_to_points(functionSpace, nodalField):
     return jax.vmap(interpolate_to_element_points, (None, 0, 0))(nodalField, functionSpace.shapes, functionSpace.mesh.conns)
 
@@ -315,15 +304,6 @@ def compute_element_field_gradient(U, coords, elemShapes, elemShapeGrads, elemVo
 def compute_quadrature_point_field_gradient(u, shapeGrad):
     dg = np.tensordot(u, shapeGrad, axes=[0,0])
     return dg
-
-
-def compute_element_field_hessian(U, elemShapeHessians, elemConnectivity):
-    elemNodalDisps = U[elemConnectivity]
-    return jax.vmap(compute_quadrature_point_field_hessian, (None, 0))(elemNodalDisps, elemShapeHessians)
-
-
-def compute_quadrature_point_field_hessian(u, shapeHess):
-    return np.tensordot(u, shapeHess, axes=[0,0])
 
 
 def interpolate_to_point(elementNodalValues, shape):

optimism/Mechanics.py
@@ -52,7 +52,9 @@ class NewmarkParameters(eqx.Module):
 
 
 def plane_strain_gradient_transformation(elemDispGrads, elemShapes, elemVols, elemNodalDisps, elemNodalCoords):
-    return vmap(tensor_2D_to_3D)(elemDispGrads)
+    def map_2D_tensor_to_3D(H):
+        return np.zeros((3,H.shape[1]+1)).at[ 0:H.shape[0], 0:2 ].set(H[:,0:2]).at[ 0:H.shape[0], 3: ].set(H[:,2:])
+    return vmap(map_2D_tensor_to_3D)(elemDispGrads)
 
 
 def volume_average_J_gradient_transformation(elemDispGrads, elemVols, pShapes):

optimism/test/test_FunctionSpace.py
@@ -170,10 +170,6 @@ class TestFunctionSpaceMultiQuadPointFixture(MeshFixture.MeshFixture):
         self.assertTrue(np.allclose(dispGrads, exact))
 
 
-    def test_field_hessian_throws(self):
-        self.assertRaises(NotImplementedError, FunctionSpace.compute_field_hessian, self.fs, self.U)
-
-
     def test_integrate_constant_field_multi_point_quadrature(self):
         integralOfOne = FunctionSpace.integrate_over_block(self.fs,
                                                            self.U,
@@ -291,7 +287,7 @@ class ParameterizationTestSuite(MeshFixture.MeshFixture):
 
 class TestFunctionSpaceWithQuadraticLagrangeElementFixture(MeshFixture.MeshFixture):
     def setUp(self):
-        self.quadratureRule = QuadratureRule.create_quadrature_rule_on_triangle(degree=1)
+        self.quadratureRule = QuadratureRule.create_quadrature_rule_on_triangle(degree=2)
 
         # mesh
         self.Nx = 2
@@ -319,12 +315,17 @@ class TestFunctionSpaceWithQuadraticLagrangeElementFixture(MeshFixture.MeshFixtu
                                  [self.targetDispHessian[1,0]*x[0] + self.targetDispHessian[1,2]*x[1], self.targetDispHessian[1,2]*x[0] + self.targetDispHessian[1,1]*x[1]]])
             return jax.vmap(compute_qp_displacement_grad, (None, 0))(elemNodalX, elemShapeFunctions)
 
-        dispGrads = FunctionSpace.compute_field_gradient(self.fs, self.U)
+        from optimism import Mechanics
+        dispGradsAndHessians = FunctionSpace.compute_field_gradient(self.fs, self.U, modify_element_gradient=Mechanics.plane_strain_gradient_transformation)
+
+        # check displacement gradients
+        dispGrads = dispGradsAndHessians[:,:,0:2,0:2]
         shapeOnRef = Interpolants.compute_shapes(self.mesh.parentElement, self.quadratureRule.xigauss)
         exact = jax.vmap(compute_expected_displacement_grads, (None, 0, None))(self.mesh.coords, self.mesh.conns, shapeOnRef.values)
         self.assertTrue(np.allclose(dispGrads, exact))
 
-        dispHessians = FunctionSpace.compute_field_hessian(self.fs, self.U)
+        # check displacement hessians
+        dispHessians = dispGradsAndHessians[:,:,0:2,3:]
         nElements = Mesh.num_elements(self.mesh)
         npts = self.quadratureRule.xigauss.shape[0]
         exact = np.tile(self.targetDispHessian, (nElements, npts, 1, 1))