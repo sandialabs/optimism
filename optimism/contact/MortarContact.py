fixes nans from average normal; zeros gaps for normals that are not pointing towards each other; moves exclusion of opposite face normals to neighbor search

optimism/contact/MortarContact.py
@@ -16,12 +16,16 @@ def compute_normal(edgeCoords):
 def compute_average_normal(edgeA : jnp.array, edgeB : jnp.array) -> jnp.array:
     nA = compute_normal(edgeA)
     nB = compute_normal(edgeB)
-    normal = nA - nB
+    normal = jnp.where(nA != nB, nA - nB, nA)
     return normal / jnp.linalg.norm(normal)
 
 
 def compute_normal_from_a(edgeA : jnp.array, edgeB : jnp.array) -> jnp.array:
     return compute_normal(edgeA)
+
+
+def normals_are_facing(edgeA, edgeB):
+    return jnp.dot(compute_normal(edgeA), compute_normal(edgeB)) < 0.0
 
 # field utilities
 
@@ -31,14 +35,6 @@ def eval_linear_field_on_edge(field, xi):
 
 def smooth_linear(xi, l):                
     return jnp.where( xi < l, 0.5*xi*xi/l, jnp.where(xi > 1.0-l, 1.0-l-0.5*(1.0-xi)*(1.0-xi)/l, xi-0.5*l) )
-
-# utilities for clamping gap values
-
-def no_clamp(xiA, g):
-    return xiA
-
-def clamp_max_penetration(xiA, g, max_penetration_depth):
-    return jnp.where(g < -max_penetration_depth, jnp.nan, xiA)
 
 # some actual mortar integrals
 
@@ -52,10 +48,7 @@ def compute_intersection(edgeA, edgeB, f_common_normal):
         xig = jnp.linalg.solve(M,r)
         return xig[0], xig[1]
 
-    normal = jax.lax.cond(jnp.dot(compute_normal(edgeA), compute_normal(edgeB)) == 0.0, 
-                        lambda eA, eB: jnp.array([jnp.nan, jnp.nan]),
-                        lambda eA, eB: f_common_normal(eA, eB), 
-                        edgeA, edgeB)
+    normal = f_common_normal(edgeA, edgeB)
 
     xiBs1, gs1 = jax.vmap(compute_xi, (0,None,None))(edgeA, edgeB, normal)
     xiAs2, gs2 = jax.vmap(compute_xi, (0,None,None))(edgeB, edgeA,-normal)
@@ -93,10 +86,8 @@ def integrate_with_mortar(edgeA : jnp.array,
                           edgeB : jnp.array, 
                           f_common_normal : Callable[[jnp.array,jnp.array],jnp.array],
                           func_of_xiA_xiB_g : Callable[[float,float,float],float],
-                          relativeSmoothingSize : float,
-                          f_clamp_gaps: Callable):
+                          relativeSmoothingSize : float):
     xiA,xiB,g = compute_intersection(edgeA, edgeB, f_common_normal)
-    xiA = f_clamp_gaps(xiA, g)
     branches = [lambda : integrate_with_active_mortar(xiA, xiB, g, 
                                                       jnp.linalg.norm(edgeA[0] - edgeA[1]), 
                                                       jnp.linalg.norm(edgeB[0] - edgeB[1]),
@@ -104,13 +95,12 @@ def integrate_with_mortar(edgeA : jnp.array,
                                                       relativeSmoothingSize),
                 lambda : 0.0]
 
-    return jax.lax.switch(1*jnp.any(jnp.isnan(xiA)), branches) #jax.lax.cond(jnp.any(jnp.isnan(xiA)), branches[1], branches[0]) #
+    return jax.lax.switch(1*jnp.logical_not(normals_are_facing(edgeA, edgeB)), branches) 
 
 
 def assembly_mortar_integral(coords, disp, segmentConnsA, segmentConnsB, neighborList, 
                              f_average_normal : Callable,
-                             f_integrand : Callable,
-                             f_clamp_gaps : Callable):
+                             f_integrand : Callable):
     def compute_nodal_gap_area(segB, neighborSegsA):
         nodeLeft = segB[0]
         nodeRight = segB[1]
@@ -119,8 +109,8 @@ def assembly_mortar_integral(coords, disp, segmentConnsA, segmentConnsB, neighbo
             coordsSegB = coords[segB] + disp[segB]
             coordsSegA = coords[segA] + disp[segA]
 
-            gapAreaLeft = integrate_with_mortar(coordsSegB, coordsSegA, f_average_normal, lambda xiA, xiB, gap: f_integrand(gap) * (1.0-xiA), 1e-9, f_clamp_gaps)
-            gapAreaRight = integrate_with_mortar(coordsSegB, coordsSegA, f_average_normal, lambda xiA, xiB, gap: f_integrand(gap) * xiA, 1e-9, f_clamp_gaps)
+            gapAreaLeft = integrate_with_mortar(coordsSegB, coordsSegA, f_average_normal, lambda xiA, xiB, gap: f_integrand(gap) * (1.0-xiA), 1e-9)
+            gapAreaRight = integrate_with_mortar(coordsSegB, coordsSegA, f_average_normal, lambda xiA, xiB, gap: f_integrand(gap) * xiA, 1e-9)
             return gapAreaLeft, gapAreaRight
 
         gapAreaLeft, gapAreaRight = jax.vmap(compute_quantities_for_segment_pair, (None,0))(segB, neighborSegsA)
@@ -136,17 +126,18 @@ def assembly_mortar_integral(coords, disp, segmentConnsA, segmentConnsB, neighbo
 
 
 def assemble_area_weighted_gaps(coords, disp, segmentConnsA, segmentConnsB, neighborList, f_average_normal : Callable):
-    return assembly_mortar_integral(coords, disp, segmentConnsA, segmentConnsB, neighborList, f_average_normal, lambda gap : gap, no_clamp)
-
-
-def assemble_area_weighted_clamped_gaps(coords, disp, segmentConnsA, segmentConnsB, neighborList, f_average_normal : Callable, f_clamp_gaps : Callable):
-    return assembly_mortar_integral(coords, disp, segmentConnsA, segmentConnsB, neighborList, f_average_normal, lambda gap : gap, f_clamp_gaps)
+    return assembly_mortar_integral(coords, disp, segmentConnsA, segmentConnsB, neighborList, f_average_normal, lambda gap : gap)
 
 
 def assemble_nodal_areas(coords, disp, segmentConnsA, segmentConnsB, neighborList, f_average_normal : Callable):
-    return assembly_mortar_integral(coords, disp, segmentConnsA, segmentConnsB, neighborList, f_average_normal, lambda gap : 1.0, no_clamp)
+    return assembly_mortar_integral(coords, disp, segmentConnsA, segmentConnsB, neighborList, f_average_normal, lambda gap : 1.0)
 
 # utilities for setting up mortar contact data structures
+
+def minimum_squared_distance(xs1, xs2):
+    dists = jax.vmap( lambda x: jax.vmap( lambda y: (x-y)@(x-y) )(xs1) ) (xs2)    
+    return jnp.min(dists)
+
 
 @partial(jax.jit, static_argnums=(4,))
 def get_closest_neighbors(edgeSetA : jnp.array,
@@ -154,26 +145,54 @@ def get_closest_neighbors(edgeSetA : jnp.array,
                           mesh : Mesh.Mesh,
                           disp : jnp.array,
                           maxNeighbors : int):
+    def min_dist_squared(edge1, edge2, coords, disp):
+        xs1 = coords[edge1] + disp[edge1]
+        xs2 = coords[edge2] + disp[edge2]
+        return minimum_squared_distance(xs1, xs2)
+    
+    return _neighbor_search(edgeSetA, edgeSetB, mesh, disp, maxNeighbors, min_dist_squared)
+
+
+@partial(jax.jit, static_argnums=(4,5))
+def get_closest_neighbors_for_self_contact(edgeSetA : jnp.array,
+                                           edgeSetB : jnp.array,
+                                           mesh : Mesh.Mesh,
+                                           disp : jnp.array,
+                                           maxNeighbors : int,
+                                           maxPenetrationDistance : float):
     def edges_are_adjacent(edge1, edge2):
         return (edge1[0] == edge2[0]) | \
                (edge1[0] == edge2[1]) | \
                (edge1[1] == edge2[0]) | \
                (edge1[1] == edge2[1])
+    
+    def edge_is_penetrating_beyond_max_distance(xs1, xs2, minSqiaredDistance):
+        return jnp.logical_and(normals_are_facing(xs1, xs2),
+                               minSqiaredDistance > maxPenetrationDistance**2)
 
-    def compute_distances(edge1, edge2, coords, disp):
+    def min_dist_conditional(edge1, edge2, coords, disp):
         xs1 = coords[edge1] + disp[edge1]
         xs2 = coords[edge2] + disp[edge2]
-        dists = jax.vmap( lambda x: jax.vmap( lambda y: (x-y)@(x-y) )(xs1) ) (xs2)    
-        return jnp.min(dists)
-
-    def min_dist_squared(edge1, edge2, coords, disp):
-        return jax.lax.cond(edges_are_adjacent(edge1, edge2), 
-                            lambda e1, e2: jnp.nan, 
-                            lambda e1, e2: compute_distances(e1, e2, coords, disp), 
+        minSqiaredDistance = minimum_squared_distance(xs1, xs2)
+        excludeEdges = jnp.logical_or(edges_are_adjacent(edge1, edge2), 
+                                      edge_is_penetrating_beyond_max_distance(xs1, xs2, minSqiaredDistance))
+        return jax.lax.cond(excludeEdges, 
+                            lambda e1, e2: jnp.inf, 
+                            lambda e1, e2: minSqiaredDistance, 
                             edge1, edge2)
     
+    return _neighbor_search(edgeSetA, edgeSetB, mesh, disp, maxNeighbors, min_dist_conditional)
+
+
+def _neighbor_search(edgeSetA : jnp.array,
+                     edgeSetB : jnp.array,
+                     mesh : Mesh.Mesh,
+                     disp : jnp.array,
+                     maxNeighbors : int,
+                     f_min_distance : Callable):
+    
     def get_close_edge_indices(surfaceA, edgeB):
-        minDistsToA = jax.vmap(min_dist_squared, (0,None,None,None))(surfaceA, edgeB, mesh.coords, disp)
+        minDistsToA = jax.vmap(f_min_distance, (0,None,None,None))(surfaceA, edgeB, mesh.coords, disp)
         return jnp.argsort(minDistsToA)[:maxNeighbors]
     
     return jax.vmap(get_close_edge_indices, (None,0))(edgeSetA, edgeSetB) # loop over surface B, get neighbor index in A

optimism/contact/test/test_TwoBodyMortarContact.py
@@ -49,7 +49,7 @@ class TwoBodyContactFixture(MeshFixture):
                 segA = self.segmentConnsA[indexA]
                 coordsSegB = self.mesh.coords[segB] + self.disp[segB]
                 coordsSegA = self.mesh.coords[segA] + self.disp[segA]
-                return MortarContact.integrate_with_mortar(coordsSegB, coordsSegA, MortarContact.compute_average_normal, lambda xiA, xiB, gap: 1.0, 1e-9, f_clamp_gaps=MortarContact.no_clamp)
+                return MortarContact.integrate_with_mortar(coordsSegB, coordsSegA, MortarContact.compute_average_normal, lambda xiA, xiB, gap: 1.0, 1e-9)
             
             return np.sum(vmap(compute_area_for_segment_pair, (None,0))(segB, neighborSegsA))
         
@@ -84,14 +84,14 @@ class TwoBodySelfContactFixture(MeshFixture):
         m1 = self.create_mesh_and_disp(2, 4, [0.0, 1.0], [0.0, 1.0],
                                         lambda x : self.targetDispGrad.dot(x), '1')
 
-        # Have second block offest (not penetrating) to test clamping of gaps for values larger than half the block width.
-        # Clamping removes gap values computed for edges on opposite faces of a body (they shouldn't be considered as penetrating)
+        # Have second block offest (not penetrating) to test that neighbor search excludes edges on opposite faces of the same body
+        # by using maxPenetrationDistance = 1/2 the body length
         m2 = self.create_mesh_and_disp(2, 3, [1.1, 2.0], [0.0, 1.0],
                                         lambda x : self.targetDispGrad.dot(x), '2')
         
         self.mesh, _ = Mesh.combine_mesh(m1, m2)
         order=2
-        self.mesh = Mesh.create_higher_order_mesh_from_simplex_mesh(self.mesh, order=order, copyNodeSets=False)
+        self.mesh = Mesh.create_higher_order_mesh_from_simplex_mesh(self.mesh, order=order, copyNodeSets=True)
         self.disp = np.zeros(self.mesh.coords.shape)
 
         self.numNeighbors = 8 # include enough neighbors so that search includes edges on opposite faces
@@ -103,7 +103,7 @@ class TwoBodySelfContactFixture(MeshFixture):
     
 
     def test_neighbor_search_excludes_current_and_adjacent_edges(self):
-        neighborList = MortarContact.get_closest_neighbors(self.segmentConnsA, self.segmentConnsB, self.mesh, self.disp, self.numNeighbors)
+        neighborList = MortarContact.get_closest_neighbors_for_self_contact(self.segmentConnsA, self.segmentConnsB, self.mesh, self.disp, self.numNeighbors, maxPenetrationDistance=1.0)
 
         def edges_are_adjacent(edge1, edge2):
             return (edge1[0] == edge2[0]) | \
@@ -125,7 +125,7 @@ class TwoBodySelfContactFixture(MeshFixture):
         nodesB = np.unique(np.concatenate(self.segmentConnsB))
         self.assertArrayEqual(nodesA, nodesB)
 
-        neighborList = MortarContact.get_closest_neighbors(self.segmentConnsA, self.segmentConnsB, self.mesh, self.disp, self.numNeighbors)
+        neighborList = MortarContact.get_closest_neighbors_for_self_contact(self.segmentConnsA, self.segmentConnsB, self.mesh, self.disp, self.numNeighbors, maxPenetrationDistance=0.5)
 
         nodalAreaField = MortarContact.assembly_mortar_integral(self.mesh.coords, 
                                                                 self.disp, 
@@ -133,36 +133,42 @@ class TwoBodySelfContactFixture(MeshFixture):
                                                                 self.segmentConnsB, 
                                                                 neighborList, 
                                                                 MortarContact.compute_average_normal, 
-                                                                lambda gap: 1.0, 
-                                                                MortarContact.no_clamp)
-        clampedNodalAreaField = MortarContact.assembly_mortar_integral(self.mesh.coords, 
-                                                                       self.disp, 
-                                                                       self.segmentConnsA, 
-                                                                       self.segmentConnsB, 
-                                                                       neighborList, 
-                                                                       MortarContact.compute_average_normal, 
-                                                                       lambda gap: 1.0, 
-                                                                       lambda xiA, g: MortarContact.clamp_max_penetration(xiA, g, max_penetration_depth=0.5))
+                                                                lambda gap: 1.0) 
 
-        # check that area is wrong if clamping is not used (gaps computed for edges on opposite faces)
-        self.assertNotAlmostEqual(np.sum(nodalAreaField), 2.0, 8) # double counting each edge, so area is doubled
-
-        # check that area is correct if clamping is used 
-        self.assertNear(np.sum(clampedNodalAreaField), 2.0, 8) # double counting each edge, so area is doubled
-        self.assertNear(np.sum(clampedNodalAreaField), np.sum(clampedNodalAreaField[nodesA]), 14)
-        mask = np.ones(len(clampedNodalAreaField), dtype=np.int8)
+        self.assertNear(np.sum(nodalAreaField), 2.0, 8) # double counting each edge, so area is doubled
+        self.assertNear(np.sum(nodalAreaField), np.sum(nodalAreaField[nodesA]), 14)
+        mask = np.ones(len(nodalAreaField), dtype=np.int8)
         mask = mask.at[nodesA].set(0).astype(bool)
 
-        self.assertNear(np.sum(clampedNodalAreaField[mask]), 0.0, 14)
+        self.assertNear(np.sum(nodalAreaField[mask]), 0.0, 14)
 
 
-    def test_clamped_mortar_integral_computes_no_penetration(self):
-        neighborList = MortarContact.get_closest_neighbors(self.segmentConnsA, self.segmentConnsB, self.mesh, self.disp, self.numNeighbors)
+    def test_mortar_integral_computes_no_penetration(self):
+        neighborList = MortarContact.get_closest_neighbors_for_self_contact(self.segmentConnsA, self.segmentConnsB, self.mesh, self.disp, self.numNeighbors, maxPenetrationDistance=0.5)
         nodalGapField = MortarContact.assemble_area_weighted_gaps(self.mesh.coords, self.disp, self.segmentConnsA, self.segmentConnsB, neighborList, MortarContact.compute_average_normal)
-        clampedNodalGapField = MortarContact.assemble_area_weighted_clamped_gaps(self.mesh.coords, self.disp, self.segmentConnsA, self.segmentConnsB, neighborList, MortarContact.compute_average_normal, lambda xiA, g: MortarContact.clamp_max_penetration(xiA, g, max_penetration_depth=0.5))
 
-        self.assertFalse(np.all(nodalGapField >= 0.0)) # negative gap values computed since edges on opposite faces are included
-        self.assertTrue(np.all(clampedNodalGapField >= 0.0)) # negative gap values computed since edges on opposite faces are included
+        self.assertTrue(np.all(nodalGapField >= 0.0)) # negative gap values computed since edges on opposite faces are included
+    
+
+    def test_penalty_potential_gradient_contains_no_nan(self):
+        def penalty_contact_potential(U, neighborList):
+            gaps = MortarContact.assemble_area_weighted_gaps(self.mesh.coords, 
+                                                             U, 
+                                                             self.segmentConnsA, 
+                                                             self.segmentConnsB, 
+                                                             neighborList, 
+                                                             MortarContact.compute_average_normal)
+            active_gaps = np.where(gaps > 0, 0.0, gaps)
+            return 0.5*np.tensordot(active_gaps, active_gaps, axes=1)
+        
+
+        index = (self.mesh.nodeSets['right1'], 0) 
+        disp = np.zeros(self.mesh.coords.shape)
+        disp = disp.at[index].set(0.2) # set displacement to cause penetration
+        neighborList = MortarContact.get_closest_neighbors_for_self_contact(self.segmentConnsA, self.segmentConnsB, self.mesh, disp, self.numNeighbors, maxPenetrationDistance=0.5)
+
+        penalty_grad = grad(penalty_contact_potential,0)(disp, neighborList)
+        self.assertFalse(np.any(np.isnan(penalty_grad)))
 
 
 if __name__ == '__main__':