9b4c7375975298e5e79ce5cd8a2e804d2fa850c3
Brandon Talamini <talamini1@llnl.gov>
2025-06-25 08:36:34 -0700

    WIP Allow gradients of fields in kernel function

--- optimism/NewFunctionSpace.py
+++ optimism/NewFunctionSpace.py
@@ -5,6 +5,7 @@ import jax.numpy as np
 from optimism import Interpolants
 from optimism import Mesh
 from optimism import QuadratureRule
+from optimism import VTKWriter
 
 class Field(abc.ABC):
     pass
@@ -34,6 +35,9 @@ class FEField(Field):
     def interpolate(self, field, shape, conn):
         e_vector = field[conn]
         return shape@e_vector
+
+    def interpolate_gradient(self, field, dshape, conn):
+        return jax.vmap(lambda ev, dshp: np.tensordot(ev, dshp, (0, 0)), (None, 0))(field[conn], dshape)
 
     def compute_shape_functions(self, points):
         return Interpolants.compute_shapes(self.element, points)
@@ -78,12 +82,13 @@ class FunctionSpace2:
     def evaluate(self, f, *fields):
         f_vmap_axis = None
         conns_vmap_axis = 0
-        compute_element_values = jax.vmap(self._evaluate_on_element, (f_vmap_axis, conns_vmap_axis) + tuple(space.element_axis for space in self._spaces))
-        return compute_element_values(f, self._mesh.conns, *fields)
+        compute_values = jax.vmap(self._evaluate_on_element, (f_vmap_axis, conns_vmap_axis) + tuple(space.element_axis for space in self._spaces))
+        return compute_values(f, self._mesh.conns, *fields)
     
     def _evaluate_on_element(self, f, el_conn, *fields):
         f_args = [z[0].interpolate(z[1], z[2].values, el_conn) for z in zip(self._spaces, fields, self._shapes)]
-        f_batch = jax.vmap(f, tuple(space.quadpoint_axis for space in self._spaces))
+        f_args += [self._spaces[0].interpolate_gradient(fields[0], self._shapes[0].gradients, el_conn)]
+        f_batch = jax.vmap(f, tuple(space.quadpoint_axis for space in self._spaces) + (0,))
         return f_batch(*f_args)
 
 
@@ -101,8 +106,6 @@ if __name__ == "__main__":
     spaces = [u_space, internal_variable_space, time_space]
 
     fs = FunctionSpace2([u_space, internal_variable_space, time_space], mesh, quad_rule)
-    # fs = FunctionSpace2([u_space], mesh, quad_rule)
-    # print(f"{fs._shapes[0].values}")
 
     def f(u, Q, t):
         return np.dot(u, u)*Q[0]*t
@@ -111,13 +114,20 @@ if __name__ == "__main__":
     print(f"{U=}")
     Q = 2.0*np.ones((Mesh.num_elements(mesh), len(quad_rule), 1))
     t = 1.0
+
+    # print(f"f() = {f(np.array([1.0, 0.0]), Q[0, 0], 1.0)}")
+
+    # val = fs.evaluate(f, U, Q, t)
+    # print(f"func vals shape = {val.shape}")
+    # print(f"elements = {Mesh.num_elements(mesh)}, quad points = {len(quad_rule)}")
+    # print(f"{val=}")
+
+    # writer = VTKWriter.VTKWriter(mesh, "new_fs")
+    # writer.write()
+
+    def g(u, Q, t, du):
+        jax.debug.print("du={du}", du=du)
+        return np.tensordot(du, du)
     
-    interpolated_values = time_space.interpolate(t, None, None)
-    print(f"{interpolated_values=}")
-
-    print(f"f() = {f(np.array([1.0, 0.0]), Q[0, 0], 1.0)}")
-
-    val = fs.evaluate(f, U, Q, t)
-    print(f"func vals shape = {val.shape}")
-    print(f"elements = {Mesh.num_elements(mesh)}, quad points = {len(quad_rule)}")
+    val = fs.evaluate(g, U, Q, t)
     print(f"{val=}")
