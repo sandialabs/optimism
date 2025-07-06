1c7631af10c5610e53d93305772972fcb5a660d5
Brandon Talamini <talamini1@llnl.gov>
2025-07-06 07:30:24 -0700

    Imrpove clarity of tests

--- optimism/test/test_FieldEvaluator.py
+++ optimism/test/test_FieldEvaluator.py
@@ -10,28 +10,8 @@ class TestBasics:
     mesh = Mesh.construct_structured_mesh(2, 2, [0.0, 3.0], [0.0, 2.0], p)
     quad_rule = QuadratureRule.create_quadrature_rule_on_triangle(2)
 
-    def test_area(self):
-        spaces = [PkField(self.p, self.dim)]
-        inputs = [Value(0)]
-        field_evaluator = FieldEvaluator(spaces, inputs, self.mesh, self.quad_rule)
-        U = np.zeros_like(self.mesh.coords)
-        def f(u):
-            return 1.0
-        area = field_evaluator.integrate(f, self.mesh.coords, U)
-        assert pytest.approx(area) == 6
-    
-    def test_area_another_way(self):
-        spaces = [PkField(self.p, self.dim)]
-        inputs = [Gradient(0)]
-        field_evaluator = FieldEvaluator(spaces, inputs, self.mesh, self.quad_rule)
-        
-        def f(dXdX):
-            return np.trace(dXdX)/self.dim
-        
-        area = field_evaluator.integrate(f, self.mesh.coords, self.mesh.coords)
-        assert pytest.approx(area) == 6
-    
-    def test_gradient_interpolation(self):
+    def test_gradient_evaluation(self):
+        "Check the gradient of an affine field"
         p = 2
         spaces = [PkField(p, self.dim)]
         inputs = [Gradient(0)]
@@ -49,4 +29,30 @@ class TestBasics:
 
         for H in disp_grads.reshape(-1, 2, 2):
             assert pytest.approx(H) == target_disp_grad
+
+    def test_trivial_integral(self):
+        spaces = [PkField(self.p, self.dim)]
+        inputs = [Value(0)]
+        field_evaluator = FieldEvaluator(spaces, inputs, self.mesh, self.quad_rule)
+        U = np.zeros_like(self.mesh.coords)
+        def f(u):
+            return 1.0
+        area = field_evaluator.integrate(f, self.mesh.coords, U)
+        assert pytest.approx(area) == 6
+    
+    def test_integral_with_one_nodal_field(self):
+        "Computes area in a non-trivial way, checking consistency of gradient and integral operators."
+        spaces = [PkField(self.p, self.dim)]
+        POSITION = 0
+        # We're taking the gradient of position, which is just the identity tensor
+        inputs = [Gradient(POSITION)]
+        field_evaluator = FieldEvaluator(spaces, inputs, self.mesh, self.quad_rule)
+
+        def f(dXdX):
+            # dXdX == identity
+            return np.trace(dXdX)/self.dim
+        
+        area = field_evaluator.integrate(f, self.mesh.coords, self.mesh.coords)
+        assert pytest.approx(area) == 6
+
 
